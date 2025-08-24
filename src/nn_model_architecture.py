11ef40043894b4a223ea043608e9397a87b72182
Waheed Ur rahman <waheedkhan957759@gmail.com>
2025-08-24 14:55:51 +0500

    update model architecture

--- src/nn_model_architecture.py
+++ src/nn_model_architecture.py
@@ -5,28 +5,71 @@ class Model(nn.Module):
   def __init__(self, in_features=9, h1=64, h2=128, h3=128, h4=64, h5=14,  out_features=7):
     super().__init__()
     self.fc1 = nn.Linear(in_features, h1)
-    self.fc2 = nn.Linear(h1, h2)
-    self.fc3 = nn.Linear(h2, h3)
-    self.fc4 = nn.Linear(h3, h4)
-    self.fc5 = nn.Linear(h4, h5)
-    self.out = nn.Linear(h5, out_features)
-    self.dropout = nn.Dropout(0.2) # Dropout layer to prevent overfitting
+    self.bn1 = nn.BatchNorm1d(h1)
 
-    # You can also use nn.Sequential to define the network in a more compact way
-    # but then you will have to change the forward method accordingly
-    '''self.network = nn.sequential(
-      nn.Linear(in_features, h1),
-      nn.Linear(h1, h2),
-      nn.Linear(h2, out_features),
-    ) '''
+    self.fc2 = nn.Linear(h1, h2)
+    self.bn2 = nn.BatchNorm1d(h2)
+
+    self.fc3 = nn.Linear(h2, h3)
+    self.bn3 = nn.BatchNorm1d(h3)
+
+    self.fc4 = nn.Linear(h3, h4)
+    self.bn4 = nn.BatchNorm1d(h4)
+
+    self.fc5 = nn.Linear(h4, h5)
+    self.bn5 = nn.BatchNorm1d(h5)
+
+    self.out = nn.Linear(h5, out_features)
+
+    # Dropout layer to prevent overfitting
+    self.dropout = nn.Dropout(0.2) 
+ 
 
   def forward(self, x):
-    # x = self.network(x)    # you will do this if you use nn.sequential otherwise do below
-    x = F.relu(self.fc1(x))
-    x = F.relu(self.fc2(x))
-    x = F.relu(self.fc3(x))
-    x = F.relu(self.fc4(x))
-    x = F.relu(self.fc5(x))
-    x = self.out(x)
+      x = self.dropout(F.relu(self.bn1(self.fc1(x))))
+      x = self.dropout(F.relu(self.bn2(self.fc2(x))))
+      x = self.dropout(F.relu(self.bn3(self.fc3(x))))
+      x = self.dropout(F.relu(self.bn4(self.fc4(x))))
+      x = self.dropout(F.relu(self.bn5(self.fc5(x))))
+      x = self.out(x)  
 
-    return x
+      return x
+  
+
+
+# Alternative model using nn.Sequential for cleaner architecture definition
+
+  class ModelSequential(nn.Module):
+    def __init__(self, in_features=9, out_features=7):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(in_features, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(128, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(64, 14),
+            nn.BatchNorm1d(14),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(14, out_features)
+        )
+
+    def forward(self, x):
+        return self.network(x)
